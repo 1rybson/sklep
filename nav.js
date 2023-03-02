177c9f5b87338797f77444ccd2f58e84eccb982e
1rybson <126080367+1rybson@users.noreply.github.com>
2023-03-02 15:18:26 +0100

    Delete nav.js

--- nav.js
+++
@@ -1,8 +0,0 @@
-function nav() {
-    var x = document.getElementById("kategorie");
-    if (x.className === "kategorie") {
-      x.className += " responsive";
-    } else {
-      x.className = "kategorie";
-    }
-  }
